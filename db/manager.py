a4e17c59a170e084b2ec548a0acc4f2d6a47c8d4
Logan Hennessy <logan@gradient.com>
2022-02-20 19:28:56 -0800

    Add pg conn variables

--- db/manager.py
+++ db/manager.py
@@ -1,3 +1,5 @@
+import os
+
 import psycopg2.extras
 
 from core.helpers import load_query
@@ -7,10 +9,10 @@ from core.models import AttemptEvent
 def save_attempt_event(attempt_event: AttemptEvent):
     query = load_query("save_attempt_event")
     conn_params = dict(
-        dbname="",
-        user="",
-        password="",
-        host="",
+        dbname="code-coach-api",
+        user="postgres",
+        password=os.getenv("PG_PASS"),
+        host=os.getenv("PG_HOST"),
         port="5432"
     )
     with psycopg2.connect(**conn_params) as conn:
